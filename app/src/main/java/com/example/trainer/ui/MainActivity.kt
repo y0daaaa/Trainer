5b7b3c2b9a837baff3cb48b6883cd534a8328d4a
y0daaaa <7thlari@gmail.com>
2024-05-17 16:03:31 +0300

    need to fix bottom nav

--- app/src/main/java/com/example/trainer/ui/MainActivity.kt
+++ app/src/main/java/com/example/trainer/ui/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.trainer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.trainer.R
 import com.example.trainer.databinding.ActivityMainBinding
+import com.example.trainer.ui.home.HomeFragment
+import com.example.trainer.ui.profile.ProfileFragment
+import com.example.trainer.ui.statistics.StatisticsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +18,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+
+                R.id.home_nav -> replaceFragment(HomeFragment())
+                R.id.statistic_nav -> replaceFragment(StatisticsFragment())
+                R.id.profile_nav -> replaceFragment(ProfileFragment())
+
+
+                else -> {
+
+
+
+                }
+
+            }
+
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
