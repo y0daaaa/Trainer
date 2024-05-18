9ee8b69e95ccb7cd340acab11bd8a001cd188295
y0daaaa <7thlari@gmail.com>
2024-05-18 19:10:36 +0300

    fix some

--- app/src/main/java/com/example/trainer/ui/MainActivity.kt
+++ app/src/main/java/com/example/trainer/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.trainer.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -23,6 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.statisticsFragment, R.id.profile_nav -> showBottomNavigationView()
+                else -> hideBottomNavigationView()
+            }
+        }
+
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.home_nav -> {
@@ -40,5 +48,13 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    private fun showBottomNavigationView() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNavigationView() {
+        binding.bottomNavigationView.visibility = View.GONE
     }
 }
