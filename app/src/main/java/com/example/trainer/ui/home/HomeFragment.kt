7736cfbb8f7e0d3a8ebe9975b451efb927124ba5
y0daaaa <7thlari@gmail.com>
2024-05-22 16:43:11 +0300

    release version 1

--- app/src/main/java/com/example/trainer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trainer/ui/home/HomeFragment.kt
@@ -4,22 +4,43 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.trainer.R
 import com.example.trainer.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
 
-    private val viewModel by viewModels<HomeViewModel> ( )
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentHomeBinding.bind(view)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.stateFlow.collect { state ->
+                    // Показати помилку
+                    state.errorMessage?.let {
+                        showMessage(it)
+                    }
+                    // Перехід на екран авторизації, якщо користувач не авторизований
+                    if (state.userNotAuth || state.logoutDone) {
+                        findNavController().navigate(R.id.action_global_sign_in_screen)
+                    }
+                    // Відобразити дані користувача, якщо потрібно
+                }
+            }
+        }
 
         binding.lightTablet.setOnClickListener {
             findNavController().navigate(R.id.action_global_lightFragment)
@@ -32,5 +53,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         binding.hardTablet.setOnClickListener {
             findNavController().navigate(R.id.action_global_hardFirstFragment)
         }
+    }
+
+    private fun showMessage(message: String) {
+        view?.let { Snackbar.make(it, message, Snackbar.LENGTH_SHORT).show() }
+        viewModel.userMessageShown()
     }
 }
--- app/src/main/java/com/example/trainer/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/trainer/ui/home/HomeViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.trainer.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.trainer.data.UserSession
 import com.example.trainer.data.UsersDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,5 +19,47 @@ class HomeViewModel @Inject constructor(
     companion object {
         const val TAG = "HomeViewModel"
     }
-}
 
+    data class State(
+        val userNotAuth: Boolean = false,
+        val logoutDone: Boolean = false,
+        val errorMessage: String? = null
+    )
+
+    private val _stateFlow = MutableStateFlow(State())
+    val stateFlow = _stateFlow.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val username = session.getAuthUser()
+            if (username == null) {
+                showMessage("Користувач не авторизований")
+                signalUserNotAuth()
+                return@launch
+            }
+            val user = userDao.fetchUser(username)
+            if (user == null) {
+                showMessage("Користувач не зареєстрований")
+                signalUserNotAuth()
+                return@launch
+            }
+        }
+    }
+
+    fun logout() {
+        session.logout()
+        _stateFlow.update { it.copy(logoutDone = true) }
+    }
+
+    fun userMessageShown() {
+        _stateFlow.update { it.copy(errorMessage = null) }
+    }
+
+    private fun showMessage(message: String) {
+        _stateFlow.update { it.copy(errorMessage = message) }
+    }
+
+    private fun signalUserNotAuth() {
+        _stateFlow.update { it.copy(userNotAuth = true) }
+    }
+}
